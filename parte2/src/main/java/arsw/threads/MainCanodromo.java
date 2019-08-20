fa12023b88689517ebba13452caad995a2c51dd7
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-08-20 18:24:03 -0500

    ganador arreglado

--- parte2/src/main/java/arsw/threads/MainCanodromo.java
+++ parte2/src/main/java/arsw/threads/MainCanodromo.java
@@ -2,6 +2,8 @@ package arsw.threads;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 
@@ -22,48 +24,53 @@ public class MainCanodromo {
         can.setStartAction(
                 new ActionListener() {
 
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-						//como acción, se crea un nuevo hilo que cree los hilos
-                        //'galgos', los pone a correr, y luego muestra los resultados.
-                        //La acción del botón se realiza en un hilo aparte para evitar
-                        //bloquear la interfaz gráfica.
-                        ((JButton) e.getSource()).setEnabled(false);
-                        new Thread() {
-                            public void run() {
-                                for (int i = 0; i < can.getNumCarriles(); i++) {
-                                    //crea los hilos 'galgos'
-                                    galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
-                                    //inicia los hilos
-                                    galgos[i].start();
-
-                                }
-                               
-				can.winnerDialog(reg.getGanador(),reg.getUltimaPosicionAlcanzada() - 1); 
-                                System.out.println("El ganador fue:" + reg.getGanador());
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                //como acción, se crea un nuevo hilo que cree los hilos
+                //'galgos', los pone a correr, y luego muestra los resultados.
+                //La acción del botón se realiza en un hilo aparte para evitar
+                //bloquear la interfaz gráfica.
+                ((JButton) e.getSource()).setEnabled(false);
+                new Thread() {
+                    public void run() {
+                        for (int i = 0; i < can.getNumCarriles(); i++) {
+                            //crea los hilos 'galgos'
+                            galgos[i] = new Galgo(can.getCarril(i), "" + i, reg);
+                            //inicia los hilos
+                            galgos[i].start();
+                        }
+                         for (int i = 0; i < can.getNumCarriles(); i++) {
+                            try {
+                                galgos[i].join();
+                            } catch (InterruptedException ex) {
+                                Logger.getLogger(MainCanodromo.class.getName()).log(Level.SEVERE, null, ex);
                             }
-                        }.start();
-
+                         }
+                        can.winnerDialog(reg.getGanador(), reg.getUltimaPosicionAlcanzada() - 1);
+                        System.out.println("El ganador fue:" + reg.getGanador());
                     }
-                }
+                }.start();
+
+            }
+        }
         );
 
         can.setStopAction(
                 new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("Carrera pausada!");
-                    }
-                }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Carrera pausada!");
+            }
+        }
         );
 
         can.setContinueAction(
                 new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("Carrera reanudada!");
-                    }
-                }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Carrera reanudada!");
+            }
+        }
         );
 
     }
